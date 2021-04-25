ea5ad3fd4a3233b76619b4b0c8108e78feee8267
Wouterr0 <ruiterwouter@gmail.com>
2021-04-25 15:06:00 +0200

    Did a lot of stuff

--- chatbot.py
+++ chatbot.py
@@ -4,17 +4,20 @@
 import sys
 import random
 import re
+from collections import OrderedDict
 
 import speech_recognition as sr
 from gtts import gTTS
 from playsound import playsound
 
 
+DEBUG = False
 
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Calibrating")
-    r.adjust_for_ambient_noise(source)
+if not DEBUG:
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Calibrating")
+        r.adjust_for_ambient_noise(source)
 
 
 def red(s):
@@ -42,80 +45,107 @@ def listen(text):
 
 def say(text):
     # print(f"saying {text=}")
-    tts = gTTS(text)
+    tts = gTTS(text, tld="co.uk", lang="en")
     with open(".tmp.mp3", 'wb') as f:
         tts.write_to_fp(f)
     playsound(".tmp.mp3")
-    
 
 
 
-
-static_responses = {
-    (r"(hi|hello)(\schatbot)?",):  ["Hi", "Hello", "G'day mate", "wazzup"],
-    ("no",): ["Ok.", "Maybe not then."],
-    ("what's up", "how do you feel", "how are you(\s\w+)?"): ["Great!", "Fantastic!", "I'm good, thanks! How about you?"],
-}
-
-
-
-dynamic_responses = {
-    r"good (\w+)":      r"Good \g<1>!",
-    r"i think (.*)":    r"What makes you think \g<1>?",
-    r"i (.*) you":      r"Why do you \g<1> me?",
-    r"i feel (.*)":     r"Why do you feel \g<1>?",
-    r"i am (.*)":       r"Why are you \g<1>?",
-    r"i'm (.*)":       r"Why are you \g<1>?",
-    r"i want to (.*)":  r"Why do you want to \g<1>?",
-    r"i like (?!you$)(.*)": r"Why do you like \g<1>?",
-}
+responses = OrderedDict([
+    (r"(hi|hello)(\schatbot)?", [
+        "Hi",
+        "Hello",
+        "G'day mate",
+        "wazzup"]),
+    (r"no", [
+        "Ok.",
+        "Maybe not then."]),
+    (r"(what's up|how do you feel|how are you)(\s\w+)?", [
+        "Great! How about you?",
+        "Fantastic! How are you?",
+        "I'm good, thanks! How are you today?"]),
+    (r"good (\w+)", [
+        r"Good \g<1>!"]),
+    (r"i think (.*)", [
+        r"What makes you think \g<1>?"]),
+    (r"i (\w+) you", [
+        r"Why do you \g<1> me?"]),
+    (r"i feel (.*)", [
+        r"Why do you feel \g<1>?"]),
+    (r"(i am|i'm) (.*)", [
+        r"Why are you \g<2>?"]),
+    (r"i want to (.*)", [
+        r"Why do you want to \g<1>?"]),
+    (r"i like (?!you$)(.*)", [
+        r"Why do you like \g<1>?"]),
+    (r"what's your (.*)", [
+        r"I don't have a \g<1>."]),
+    (r"would you like to (.*)", [
+        r"I don't know."]),
+    (r"are you (.*)", [
+        r"Last time I checked I wasn't \g<1>.",
+        r"I don't think I am \g<1>.",
+        r"I don't know if i'm \g<1>. Are you?"]),
+    (r"why (am|are|is|was|were|will)(n't)? (.*)", [
+        r"I have absolutely no idea why \g<3> \g<1>\g<2>."]),
+    (r"because (.*)", [
+        r"Aah, right. Because \g<1>."]),
+    (r"stop (.*)", [
+        r"I will never stop \g<1>."]),
+    (r".+", [
+        r"What do you mean \g<0>."]),
+])
 
 
 connectors = ["I'd like to hear more.", "That sounds interesting.", "Can you explain more?", "Tell me more!", "Nice! How can I help you?"]
 
-welcome_messages = ["Hello!", "What's up?", "Hi!", "Welcome to Wouter and Ruben's great AI. How can I help you?"]
+welcome_messages = ["Hello!", "What's up?", "Hi!", "Welcome to Wouter's great AI. How can I help you?"]
 
 
 
-pronoun_patterns = {
-    r"(\s|^)i\s": " you ",
-    r"(\s|^)my\s": " your ",
-    r"(\s|^)you\s": " me ",
-    r"(\s|^)your\s": " my ",
-}
+pronoun_patterns = OrderedDict([
+    (r"(\s|^)you\s",   " me "),
+    (r"(\s|^)your\s",  " my "),
+    (r"(\s|^)(I|i)\s", " you "),
+    (r"(\s|^)my\s",    " your "),
+    (r"(\s|^)am\s",    " are "),
+])
+
 
 def swap_pronouns(phrase):
     for pronoun_pattern in pronoun_patterns:
-        return re.sub(pronoun_pattern, pronoun_patterns[pronoun_pattern], phrase).strip()
+        phrase = re.sub(pronoun_pattern, pronoun_patterns[pronoun_pattern], phrase).strip()
+    return phrase
 
 
 def generate_response(user_phrase):
-    for pattern_group in static_responses:
-        for pattern in pattern_group:
-            match = re.fullmatch(pattern, user_phrase)
-            if match:
-                return random.choice(static_responses[pattern_group])
-    for pattern in dynamic_responses:
+    for pattern in responses:
         match = re.fullmatch(pattern, user_phrase)
         if match:
-            return swap_pronouns(re.sub(pattern, dynamic_responses[pattern], user_phrase))
+            print(red(user_phrase), red(swap_pronouns(user_phrase)), sep='\n')
+            return re.sub(pattern, random.choice(responses[pattern]), swap_pronouns(user_phrase))
     else:
         return random.choice(connectors)
 
 
 welcome_message = random.choice(welcome_messages)
-say(welcome_message)
+if not DEBUG:
+    say(welcome_message)
 print(blue(welcome_message))
 
 while True:
     while True:
         try:
+            if DEBUG:
+                raise KeyboardInterrupt
             user_phrase = listen(None)
         except KeyboardInterrupt:
             user_phrase = input(magenta("What do you want to say: "))
-        print(yellow(str(user_phrase)))
+        print(yellow(user_phrase))
         if user_phrase != None:
             break
     chatbot_response = generate_response(user_phrase.lower())
-    say(chatbot_response)
+    if not DEBUG:
+        say(chatbot_response)
     print(blue(chatbot_response))
